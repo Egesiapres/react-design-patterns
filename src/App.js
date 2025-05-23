d67257283898609f9ee9a9a3ce0df4aed98ec1bc
Umberto Pasinetti <pasinetti96@gmail.com>
2025-05-23 13:27:10 +0000

    S2 - 14. Local Storage Data Loader Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import axios from "axios";
 import { UserInfo } from "./components/user-info";
 import { DataSourceWithRender } from "./components/data-source-with-render";
+import { DataSource } from "./components/data-source";
 
 const getDataFromServer = async (url) => {
   const response = await axios.get(url);
   return response.data;
 }
 
+// !!! This f returns another f, that needs to be invoked to access the object corrisponding to the key
+// this because getData prop consumes a f
+const getDataFromLocalStorage = key => () => {
+  return localStorage.getItem(key);
+}
+
+const Message = ({ msg }) => <h1>{msg}</h1> 
+
 function App() {
   return (
     <>
       <DataSourceWithRender getData={() => getDataFromServer("/users/1")} render={(resource) => <UserInfo user={resource} />}>
       </DataSourceWithRender>
+
+      <DataSource getData={() => getDataFromLocalStorage("test")} resourceName={"msg"}>
+        <Message />
+      </DataSource>
     </>
   );
 }
