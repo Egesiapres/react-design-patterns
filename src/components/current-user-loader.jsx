9e865f6faef01c6a779316111e21b789608e53c5
Umberto Pasinetti <pasinetti96@gmail.com>
2025-05-23 10:40:29 +0000

    S2 - 9. Loader Component for CurrentUser Data

---
+++ src/components/current-user-loader.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react"
+
+// P1: Container Component Pattern
+export const CurrentUserLoader = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get("/current-user");
+
+      setUser(response.data)
+    })()
+  }, []);
+
+  // Props are passed just in some scenarios
+  
+  // React.Children: access to the children elements to perform something
+  // React.isValidElement: just if the child is valid
+  // React.cloneElement: actually attaches some extra props to the element
+  return (
+    <>
+      {React.Children.map(children, child => {
+        if (React.isValidElement(child)) {
+          return React.cloneElement(child, { user })
+        }
+
+        return child;
+      })}
+    </>
+  )
+}
