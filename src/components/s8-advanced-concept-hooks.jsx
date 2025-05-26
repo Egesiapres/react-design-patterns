9b1facb6b7fbc99a0b237d61df9cf082f2b88e29
Umberto Pasinetti <pasinetti96@gmail.com>
2025-05-26 15:53:36 +0000

    S8 - 43. useLayoutEffect

--- src/components/s8-advanced-concept-hooks.jsx
+++ src/components/s8-advanced-concept-hooks.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { createPortal } from "react-dom";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+// import { createPortal } from "react-dom";
 import "../App.css";
 // import { Child } from "./child";
 // import Counter1 from "./counter1";
@@ -17,12 +17,27 @@ import "../App.css";
 // 2. Bubbling: bottom to top
 function S8AdvancedConceptHooks() {
   const [show, setShow] = useState(false);
+  const [top, setTop] = useState(0);
+  const buttonRef = useRef(null);
 
   // const [changeShirts, setChangeShirts] = useState(false);
 
+  // like the useEffect but handles in a better way scenarios in which
+  // I'm rendering something according to the output of an operation that is happening inside the useEffect
+  useLayoutEffect(() => {
+    if (buttonRef.current === null || !show) return setTop(0);
+    const { bottom } = buttonRef.current.getBoundingClientRect();
+    setTop(bottom + 30);
+  }, [show]);
+
+  const now = performance.now();
+  while (now > performance.now() - 100) {
+    // Do something
+  }
+
   return (
     <>
-      <div
+      {/* <div
         onClickCapture={() => console.log("outer div")}
         style={{ position: "absolute", marginTop: "200px" }}
       >
@@ -37,7 +52,7 @@ function S8AdvancedConceptHooks() {
           <br />
           Click it to close.
         </Alert>
-      </div>
+      </div> */}
 
       {/* <h1>Parent Component</h1>
       <ErrorBoundary fallback={<h1>Error at Child Level</h1>}>
@@ -58,26 +73,46 @@ function S8AdvancedConceptHooks() {
         <br />
         <button onClick={() => setChangeShirts(s => !s)}>Switch</button>
       </div> */}
+
+      <button
+        ref={buttonRef}
+        onClick={() => setShow(s => !s)}
+      >
+        Show
+      </button>
+      {show && (
+        <div
+          className="tooltip"
+          style={{
+            top: `${top}px`,
+          }}
+        >
+          Some text ...
+        </div>
+      )}
     </>
   );
 }
 
 // NEVER attach elements using portals inside the body
 // possible perf issues & conflict with other libraries
-const Alert = ({ children, onClose, show }) => {
-  if (!show) return;
-  return createPortal(
-    <div
-      className="alert"
-      onClickCapture={() => {
-        onClose();
-        console.log("inner div");
-      }}
-    >
-      {children}
-    </div>,
-    document.querySelector("#alert-holder")
-  );
-};
+// const Alert = ({ children, onClose, show }) => {
+//   if (!show) return;
+//   return createPortal(
+//     <div
+//       className="alert"
+//       onClickCapture={() => {
+//         onClose();
+//         console.log("inner div");
+//       }}
+//     >
+//       {children}
+//     </div>,
+//     document.querySelector("#alert-holder")
+//   );
+// };
 
 export default S8AdvancedConceptHooks;
+
+// - review bubbling (Event Listeners)
+// - useLayoutEffect
